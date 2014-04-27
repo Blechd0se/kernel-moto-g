032adecf1843c01d4e865187ebd141433697fd7b
Blechd0se <alex.christ@hotmail.de>
2014-04-27 02:36:31 +0200

    msm: add userspace undervolting interface
    First of all, this work is havily based on this commit; https://github.com/franciscofranco/mako/commit/48e00743741efa77964781465baf35b3641ec98d
    Also thanks to @Quarx2k for giving me the right device-tree changes.
    
    This commit adds a undervolting interface for the moto g. Normally the kernel
    would set the right voltages at boot (during some sort of configuration phase).
    Although AVS works as it should, we can still undervolt a little bit more.
    A quick dmesg | grep cpr_voltage_plan_init reveals the current voltages for the cpu.
    On nearly all Moto Gs this is set to the lowest possible value (1140000 uV).
    
    Starting with the lowest value for the voltage;
    echo 1140 1140 1140 1140 1140 1140 1140 > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
    
    I set the default values to 1140, 1150, 1280 which seem to be the same as before
    AVS kicks in. I am sure we can take the AVS-process into account and set the
    correct voltages for each frequency and device during boot with this interface.
    For now i am very happy with the results so far.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -660,6 +660,22 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
+#ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
+extern ssize_t acpuclk_get_vdd_levels_str(char *buf);
+extern void acpuclk_set_vdd(const char *buf);
+
+static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf) 
+{
+	return acpuclk_get_vdd_levels_str(buf);
+}
+
+static ssize_t store_UV_mV_table(struct cpufreq_policy *policy, const char *buf, size_t count) 
+{
+	acpuclk_set_vdd(buf);
+	return count;
+}
+#endif
+
 cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
 cpufreq_freq_attr_ro(cpuinfo_max_freq);
@@ -675,6 +691,9 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+#ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
+cpufreq_freq_attr_rw(UV_mV_table);
+#endif
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -689,6 +708,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+#ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
+	&UV_mV_table.attr,
+#endif
 	NULL
 };
 
