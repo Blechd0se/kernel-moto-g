236b03dfa8d177176a3dbd9a361b825b31c9d27b
alin23 <alin.p32@gmail.com>
2014-05-31 12:29:23 +0300

    proximity: power off the sensor if dt2w/s2w is disabled
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- drivers/input/misc/ct406.c
+++ drivers/input/misc/ct406.c
@@ -1486,7 +1486,7 @@ static void ct_power_suspend(struct power_suspend *h) {
 	struct ct406_data *ct = ct406_misc_data;
 	
 	ct_suspended = true;
-	if (!ct->prox_enabled) {
+	if (!ct->prox_enabled && (s2w_switch > 0 || dt2w_switch > 0)) {
 		forced = true;
 		ct406_enable_prox(ct);
 	}
@@ -1698,7 +1698,8 @@ static int ct406_probe(struct i2c_client *client,
 		goto error_revision_read_failed;
 	}
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-	ct406_enable_prox(ct);
+	if (s2w_switch > 0 || dt2w_switch > 0)
+		ct406_enable_prox(ct);
 #endif
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
