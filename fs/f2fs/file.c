a2ee4383dd9be76c62e734928bbd08d167930dac
Blechd0se <alex.christ@hotmail.de>
2014-09-10 20:03:50 +0200

    f2fs: partially revert last commit
    https://github.com/torvalds/linux/commit/6d99ba41a72c1213f2029e6fc66aa7943339b5db
    Requires a bit more of backport work, for now it seems
    not necessary.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -132,17 +132,6 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		return ret;
 	}
 
-	/*
-	 * if there is no written data, don't waste time to write recovery info.
-	 */
-	if (!is_inode_flag_set(fi, FI_APPEND_WRITE) &&
-		!exist_written_data(sbi, inode->i_ino, APPEND_INO)) {
-		if (is_inode_flag_set(fi, FI_UPDATE_WRITE) ||
-			exist_written_data(sbi, inode->i_ino, UPDATE_INO))
-			goto flush_out;
-		goto out;
-	}
-
 	/* guarantee free sections for fsync */
 	f2fs_balance_fs(sbi);
 
@@ -192,13 +181,6 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		ret = wait_on_node_pages_writeback(sbi, inode->i_ino);
 		if (ret)
 			goto out;
-
-		/* once recovery info is written, don't need to tack this */
-		remove_dirty_inode(sbi, inode->i_ino, APPEND_INO);
-		clear_inode_flag(fi, FI_APPEND_WRITE);
-flush_out:
-		remove_dirty_inode(sbi, inode->i_ino, UPDATE_INO);
-		clear_inode_flag(fi, FI_UPDATE_WRITE);
 		ret = blkdev_issue_flush(inode->i_sb->s_bdev, GFP_KERNEL, NULL);
 	}
 out:
