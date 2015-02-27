f4a73f487ad6d24efc35cb968d88ba0471879efe
Blechd0se <alex.christ@hotmail.de>
2015-02-27 22:26:31 +0100

    ramdisk: add aero scripts
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

---
+++ ramdisk/aerocheck.sh
@@ -0,0 +1,23 @@
+#!/sbin/bbx sh
+#
+# Checks for power hal and makes a backup
+#
+
+# Interactive tunables;
+echo 30000 1094000:40000 1190000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+echo 85 1094000:80 1190000:95 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+
+# Disable PoweHAL from the rom-side;
+/sbin/bbx mount -o rw,remount /system;
+if /sbin/bbx [ -e /system/lib/hw/power.msm8226.so ]; then
+  /sbin/bbx [ -e /system/lib/hw/power.msm8226.so.backup ] || /sbin/bbx cp /system/lib/hw/power.msm8226.so /system/lib/hw/power.msm8226.so.backup;
+  /sbin/bbx [ -e /system/lib/hw/power.msm8226.so ] && /sbin/bbx rm -f /system/lib/hw/power.msm8226.so;
+fi;
+
+# Enable init.d with permissions;
+if /sbin/bbx [ ! -e /system/etc/init.d ]; then
+  /sbin/bbx mkdir /system/etc/init.d;
+  /sbin/bbx chown -R root.root /system/etc/init.d;
+  /sbin/bbx chmod -R 755 /system/etc/init.d;
+fi;
+/sbin/bbx mount -o ro,remount /system;
